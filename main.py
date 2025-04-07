51bd8bc491143ce7f4948491d1be3a68b4230709
iceeth <dylan@sosinski.cc>
2025-04-06 22:32:56 -0700

    1) Defineing get_book_text function and main function

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def main ():
+    get_books_text()
+
+def get_books_text():
+    with open('./books/frankenstein.txt', encoding="utf-8") as f:
+        print(f)
+        Book = f.read()
+    print(Book)
+    
+    
+
+
+main()
